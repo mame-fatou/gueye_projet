2fe05bc1d575550f9acd018648597f41ee6e7f1d
mame-fatou <ndeyelebayame@gmail.com>
2021-04-21 16:45:13 +0200

    fichier exclusivité pour les graphes

---
+++ repertoire/exclusivite.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang>
+<HEAD>			
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+
+<link rel="stylesheet" href="style.css" type="text/css" media="screen" />
+
+
+
+</head>
+<body>
+
+	<a href="repertoire/graph_Paris_Est_2012.php">Paris Est /</a>
+	<a href="repertoire/graph_Paris_Bercy_2012.php">Paris Bercy / </a>
+	<a href="repertoire/graph_Aéroport_Charles_de_Gaulle2.php">Aéroport Charles de Gaulle / </a>
+	<a href="repertoire/graph_Paris_Nord_2012.php">Paris Nord /</a>
+	<a href="repertoire/graphe_marne_la _vallée.php">Marne la Vallée / </a>
+	<a href="repertoire/graph_Paris_St_Lazare_2013.php">Paris Saint-Lazare / </a>
+	<a href="repertoire/paris_montparnasse.php">Paris Montparnasse / </a>
+	<a href="repertoire/massy.php">Massy / </a>
+	<a href="repertoire/paris_austerlitz.php">Paris Austerlitz / </a>
+	<a href="repertoire/paris_garedelyon.php">Paris Gare de Lyon  </a>
+	
+	<p></p>
+	<a href="repertoire/graph_Lille_Europe_2013.php">Lille Europe / </a>
+	<a href="repertoire/graph_Lille_Flandres_2013.php">Lille Flandres  </a>
+	<p></p>
+	
+	<a href="repertoire/lyon-perrache.php">Lyon Perrache /</a>
+	<a href="repertoire/lyon2.php">Lyon Saint-Exupéry  </a>
+	<p></p>
+	
+	
+	<a href="repertoire/graphe_Strasbourg_2016.php">Strasbourg  </a>
+	<p></p>
+	
+	<a href="repertoire/graph_Reims_2016.php">Reims </a>
+		<p></p>
+	<a href="repertoire/rennes.php">Rennes  </a>
+		<p></p>
+	<a href="repertoire/nantes.php">Nantes  </a>
+		<p></p>
+	<a href="repertoire/bordeaux.php">Bordeaux Saint-Jean  </a>
+		<p></p>
+	<a href="repertoire/toulouse.php">Toulouse Matabiau  </a>
+		<p></p>
+	<a href="repertoire/marseille.php">Marseille  </a>
+		<p></p>
+	<a href="repertoire/toulon.php">Toulon  </a>
+		<p></p>
+	<a href="repertoire/nice.php">Nice </a>
+		<p></p>
+	
+	<a href="repertoire/valence.php">Valence  </a>
+		<p></p>
+	<a href="repertoire/montpellier.php">Montpellier  </a>
+		<p></p>
+	
+	<div class="menu">
+
+	<p> <a href="ville.php">Faire une nouvelle recherche</a></p>
+	<p> <a href="accueil.php">Page d'accueil</a></p>
+	<p><a href="deconnexion.php">Se déconnecter </a></p>
+	
+</div>
+
+</body>
+</html>

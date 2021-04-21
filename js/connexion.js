b7f4049fe947a1abe1ca38a1d609bada6cbfe321
mame-fatou <ndeyelebayame@gmail.com>
2021-04-21 16:41:41 +0200

    fichier jpgraph

---
+++ js/connexion.js
@@ -0,0 +1,19 @@
+function zoomIn(zoom)
+{
+	document.getElementById(zoom).style.width = '500px';
+	document.getElementById(zoom).style.height = '500px';
+
+}
+zoomIn('imgZoom1')
+
+            
+function zoomOut(zoom)
+{
+
+ document.getElementById(zoom).style.width = '200px';
+ document.getElementById(zoom).style.height = '200px';
+ 
+}
+zoomOut('imgZoom1')
+
+	
---
+++ repertoire/historique.php
@@ -0,0 +1,6 @@
+<html>
+<body>
+<img src="graph_1.php"/>
+<img src="graph_2.php"/>
+</body>
+</html>
